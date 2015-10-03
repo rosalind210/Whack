d5a1f842f408cc96699e97e90128395926fc84f0
rosalind210 <rnellis@brandeis.edu>
2015-10-03 18:25:41 -0400

    Login added

--- public/js/demeter.js
+++ public/js/demeter.js
@@ -32,4 +32,36 @@ function addImage(status){
 	document.body.appendChild(img);
 }
 
+// Plugin options and our code
+$("#modal_trigger").leanModal({
+        top: 100,
+        overlay: 0.6,
+        closeButton: ".modal_close"
+});
+
+$(function () {
+    // Calling Login Form
+    $("#login_form").click(function () {
+        $(".user_login").show();
+        return false;
+    });
+ 
+    // Calling Register Form
+    $("#register_form").click(function () {
+    	$(".user_login").hide();
+        $(".user_register").show();
+        $(".header_title").text('Register');
+        return false;
+    });
+ 
+    // Going back to Social Forms
+    $(".back_btn").click(function () {
+        $(".user_login").show();
+        $(".user_register").hide();
+        $(".header_title").text('Login');
+        return false;
+    });
+ 
+})
+
 </script>
