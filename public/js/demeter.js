e854266c257b314a34395a6bb58e5e802c7ebde0
rosalind210 <rnellis@brandeis.edu>
2015-10-03 22:30:08 -0400

    Update demeter.js

--- public/js/demeter.js
+++ public/js/demeter.js
@@ -113,8 +113,8 @@ $.fn.extend({
 
 // Plugin options and our code
 $("#modal_trigger").leanModal({
-  top: 100,
-  overlay: 0.6,
+  top: 0 auto;
+  overlay: 0.6;
   closeButton: ".modal_close"
 });
 
